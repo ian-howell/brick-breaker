f1c6d0139507a901a6974a59db46643148b758df
Ian Howell <ian.howell0@gmail.com>
2016-10-27 16:28:55 -0500

    Baseline for gameplay

--- main.cpp
+++ main.cpp
@@ -1,7 +1,48 @@
 #include <ncurses.h>
+#include "player.h"
+#include "helpers.h"
 
 int main()
 {
+    int rows;
+    int cols;
+    initscr();
+    /* start_color(); */
+    init_colors();
+    cbreak();
+    noecho();
+    keypad(stdscr, TRUE);
+    curs_set(0);
+
+    // Get the screen dimensions
+    getmaxyx(stdscr, rows, cols);
+
+    Player* player = new Player(rows - 2, cols / 2);
+    player->draw(WHITE);
+    refresh();
+
+    int c;
+    bool done = false;
+    while (!done)
+    {
+        c = wgetch(stdscr);
+        switch (c)
+        {
+            case KEY_LEFT:
+                player->move(LEFT);
+                break;
+            case KEY_RIGHT:
+                player->move(RIGHT);
+                break;
+            case 'q':
+                done = true;
+                break;
+        }
+        refresh();
+    }
+
+    endwin();
+    delete player;
 
     return 0;
 }
